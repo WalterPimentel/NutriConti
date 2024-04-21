62adaa89daf0c96ea5a52d9a5328de3faf6f4b3c
WalterPimentel <70056787@continental.edu.pe>
2024-04-21 18:50:35 -0500

    Agregando Páginas

--- nutricontiweb/src/App.jsx
+++ nutricontiweb/src/App.jsx
@@ -1,24 +1,28 @@
 import { Routes, Route } from "react-router";
 
 import Ordenes from "./components/paginas/Ordenes";
+import Menu from "./components/paginas/Menu";
+import NuevoPlatillo from "./components/paginas/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
 
-/* Si por supuesto seguirá siendo lo mismo, 
-Hay algunas cositas que cambiaran por ejemplo 
-los archivos en vite debe ser nombrados al final 
-con  .jsx y no solamente .js asi mismo las 
-variables de entorno seria VITE_NOMBRE_VARIABLE 
-por ultimo tus Providers y Ruta de tus paginas los 
+/* Si por supuesto seguirá siendo lo mismo, Hay algunas cositas que cambiaran por ejemplo 
+los archivos en vite debe ser nombrados al final con  .jsx y no solamente .js asi mismo las 
+variables de entorno seria VITE_NOMBRE_VARIABLE por ultimo tus Providers y Ruta de tus paginas los 
 colocaras en el App.jsx De alli todo es igual. */
 
 function App() {
-  return(
-    <div>
-      <Routes>
-        <Route path="/" element={<Ordenes/>} />
-      </Routes>  
+  return (
+    <div className=" md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5 p-6">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
-
 
 export default App;
---
+++ nutricontiweb/src/components/ui/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+const Sidebar = () => {
+  const actualLocation = useLocation().pathname;
+  const locations = [
+    { to: "/", name: "Ordenes" },
+    { to: "/menu", name: "Menú" }
+  ];
+  return (
+    <div className="md:w-1/5 xl:w-1/5 bg-gray-800">
+      <div className="p-6">
+        <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">
+          NutriConti
+        </p>
+        <p className="mt-3 text-gray-600">
+          Administra tu restaurant con las siguientes opciones:
+        </p>
+        <nav className="mt-10">
+          {locations.map((value, index) => (
+            <NavLink
+              className={
+                value.to === actualLocation
+                  ? "text-yellow-400 block hover:bg-yellow-500 hover:text-gray-900"
+                  : "p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+              }
+              key={index}
+              to={value.to}
+            >
+              {value.name}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
