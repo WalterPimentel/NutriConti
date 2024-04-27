c1588a47abbc302d93c02076e029c50b6d22974e
WalterPimentel <70056787@continental.edu.pe>
2024-04-26 19:33:55 -0500

    NutriContiApp Creada

---
+++ nutricontiapp/views/ProgresoPedido.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+const ProgresoPedido = () => {
+    return ( 
+        <Text>ProgresoPedido</Text>
+     );
+}
+ 
+export default ProgresoPedido;
