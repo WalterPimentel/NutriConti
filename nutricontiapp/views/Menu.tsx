d0f0c43d1ba804ee672c6184d6961b27d771916e
WalterPimentel <70056787@continental.edu.pe>
2024-04-28 18:09:02 -0500

    uninstall gluestack

--- nutricontiapp/views/Menu.tsx
+++ nutricontiapp/views/Menu.tsx
@@ -1,25 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React, { useContext, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import FirebaseContext from '../context/firebase/firebaseContext';
 
 //import Icon from 'react-native-vector-icons/FontAwesome6';
 import globalStyles from '../styles/global';
-
-import { config } from '@gluestack-ui/config';
-import {
-    GluestackUIProvider,
-    Button,
-    ButtonText,
-    Box, // usar como Left, Body, Container, Content
-    Divider, // usar como Sperarator
-    FlatList,
-    SectionList,
-    Text,
-    Image,
-    HStack,
-    VStack,
-} from '@gluestack-ui/themed';
 
 const Menu = () => {
 
@@ -32,55 +17,7 @@ const Menu = () => {
     }, []);
 
     return (
-        <GluestackUIProvider config={config}>
-            <Box style={globalStyles.contenedor}>
-                <Box py="$1" style={{ backgroundColor: '#FFF' }}>
-                    <FlatList
-                        data={menu}
-                        renderItem={({ item }) => (
-                            <Box
-                                borderBottomWidth="$1"
-                                $base-pl={15}
-                                $base-pr={0}
-                                $sm-pl="$4"
-                                $sm-pr="$5"
-                                py="$3"
-                            >
-                                <HStack space="md">
-                                    <Image
-                                        size="lg"
-                                        borderRadius="$xl"
-                                        source={{ uri: item.imagen }}
-                                        alt={item.nombre}
-                                    />
-                                    <VStack>
-                                        <Text
-                                            color="$coolGray800"
-                                            fontWeight="$bold"
-                                            $dark-color="$warmGray100"
-                                            numberOfLines={1}
-                                            maxWidth="80%"
-                                        >{item.nombre}</Text>
-                                        <Text
-                                            color="$coolGray600"
-                                            $dark-color="$warmGray200"
-                                            numberOfLines={2}
-                                            maxWidth="88%"
-                                        >{item.descripcion}</Text>
-                                        <Text
-                                            fontSize="$sm"
-                                            fontWeight="$bold"
-                                            alignSelf="flex-start"
-                                        >S/. {item.precio}</Text>
-                                    </VStack>
-                                </HStack>
-                            </Box>
-                        )}
-                        keyExtractor={item => item.id}
-                    />
-                </Box>
-            </Box>
-        </GluestackUIProvider>
+            <Text>Menú</Text>
     );
 };
 
--- nutricontiapp/views/NuevaOrden.tsx
+++ nutricontiapp/views/NuevaOrden.tsx
@@ -1,46 +1,17 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { config } from '@gluestack-ui/config';
+import { View, StyleSheet, Text } from 'react-native';
 import globalStyles from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/FontAwesome6';
-
-import {
-    GluestackUIProvider,
-    Button,
-    ButtonText,
-    Box,
-} from '@gluestack-ui/themed';
 
 const NuevaOrden = () => {
 
     const navigation = useNavigation();
 
     return (
-        <GluestackUIProvider
-            config={config}
-        >
-            <Box
-                style={globalStyles.contenedor}
-            >
-                <View
-                    style={[globalStyles.contenido, styles.contenido]}
-                >
-                    <Button
-                        style={globalStyles.boton}
-                        borderRadius="$full"
-                        onPress={() => navigation.navigate('Menu') }
-                    >
-                        <ButtonText
-                            style={globalStyles.botonTexto}
-                        >Nueva Orden </ButtonText>
-                        <Icon name="circle-plus" size={20} color="#000" />
-                    </Button>
-                </View>
-            </Box>
-        </GluestackUIProvider>
+       <Text>Nueva Orden</Text>
     );
 };
 
