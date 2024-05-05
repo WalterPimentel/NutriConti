4ec6dd7f9a54bb01210420f8ecbfe743938be9b4
WalterPimentel <70056787@continental.edu.pe>
2024-05-05 00:24:13 -0500

    Login integrado

--- nutricontiweb/src/components/paginas/Login.jsx
+++ nutricontiweb/src/components/paginas/Login.jsx
@@ -1,32 +1,46 @@
+import { useState, useEffect } from 'react';
 import firebase from "../../firebase";
 
 const Login = () => {
+    const [loading, setLoading] = useState(false);
+    const [allowedEmails, setAllowedEmails] = useState([]);
+
+    useEffect(() => {
+        const fetchAllowedEmails = async () => {
+            const snapshot = await firebase.firebase.firestore().collection('usuarios').get();
+            setAllowedEmails(snapshot.docs.map(doc => doc.data().correo));
+        };
+
+        fetchAllowedEmails();
+    }, []);
+
     const handleLogin = () => {
+        setLoading(true);
         const provider = new firebase.firebase.auth.GoogleAuthProvider();
 
-        firebase.firebase.auth().signInWithPopup(provider).then(async (result) => {
-            // Lista de correos electrónicos permitidos
-            const snapshot = await firebase.firebase.firestore().collection('usuarios').get();
-            const allowedEmails = snapshot.docs.map(doc => doc.data().correo);
-            
+        firebase.firebase.auth().signInWithPopup(provider).then((result) => {
             // Puedes acceder a la información del usuario aquí
             var user = result.user;
-
             if (allowedEmails.includes(user.email)) {
                 console.log(user);
             } else {
                 // Si el correo electrónico del usuario no está en la lista de correos electrónicos permitidos, cierra la sesión
                 firebase.firebase.auth().signOut();
-                alert("No tienes permiso para acceder a esta aplicación.");
+                alert('No tienes permiso para acceder a esta aplicación');
             }
+            setLoading(false);
+
         }).catch((error) => {
             console.error(error);
+            setLoading(false);
         });
     };
 
     return (
         <div>
-            <button onClick={handleLogin}>Iniciar sesión con Google</button>
+            <button onClick={handleLogin} disabled={loading}>
+                {loading ? 'Cargando...' : 'Iniciar sesión con Google'}
+            </button>
         </div>
     );
 };
