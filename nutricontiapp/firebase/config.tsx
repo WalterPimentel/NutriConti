faca83e03c0697baf7f0e944ef14b6dc8cccd8ad
WalterPimentel <70056787@continental.edu.pe>
2024-06-04 09:49:35 -0500

    Logrado: clientId agregado firebase

--- nutricontiapp/firebase/config.tsx
+++ nutricontiapp/firebase/config.tsx
@@ -6,6 +6,8 @@ const firebaseConfig = {
     messagingSenderId: '706526518435',
     appId: '1:706526518435:web:3b51b313fe5cf1e7f9b316',
     measurementId: 'G-J7X6ZP374R',
+    
+    clientId: '706526518435-s6knfgi14mdo3p6d8flmojh17b7j1ph9.apps.googleusercontent.com'
 };
 
 export default firebaseConfig;
--- nutricontiweb/src/firebase/config.jsx
+++ nutricontiweb/src/firebase/config.jsx
@@ -5,7 +5,9 @@ const firebaseConfig = {
   storageBucket: "nutriconti-429d5.appspot.com",
   messagingSenderId: "706526518435",
   appId: "1:706526518435:web:3b51b313fe5cf1e7f9b316",
-  measurementId: "G-J7X6ZP374R"
+  measurementId: "G-J7X6ZP374R",
+
+  clientId: "706526518435-s6knfgi14mdo3p6d8flmojh17b7j1ph9.apps.googleusercontent.com"
 };
 
 export default firebaseConfig;
